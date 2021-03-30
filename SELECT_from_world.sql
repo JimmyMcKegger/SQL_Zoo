8070a18d92c122cd63d2a769cbf0a9089f79c1c1
JimmyMcKegger <57632668+JimmyMcKegger@users.noreply.github.com>
2021-03-31 00:03:57 +0100

    update world

--- SELECT_from_world.sql
+++ SELECT_from_world.sql
@@ -10,3 +10,41 @@ SELECT name
 SELECT name, gdp/population
 FROM world
 WHERE population > 200000000
+
+--4
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America'
+
+--5
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+--6
+SELECT name
+FROM world
+WHERE name LIKE '%United%'
+
+--7
+SELECT name, population, area
+FROM world
+WHERE population > 250000000 OR area > 3000000
+
+--8
+SELECT name, population, area
+FROM world
+WHERE population > 250000000 XOR area > 3000000
+
+--9
+SELECT name, ROUND(population/1000000, 2), ROUND(GDP/1000000000, 2)
+FROM world
+WHERE continent = 'South America'
+
+--10
+
+--11
+
+--12
+
+--13
