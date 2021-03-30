38dfa3199932b8b2e59f81e3b64d61afae696922
JimmyMcKegger <57632668+JimmyMcKegger@users.noreply.github.com>
2021-03-30 21:49:10 +0100

    fixed files

--- SELECT_basics.sql
+++ SELECT_basics.sql
@@ -1,14 +1,11 @@
---https://sqlzoo.net/wiki/SELECT_basics
-
 --1
-SELECT name, continent, population FROM world
+SELECT population FROM world
+  WHERE name = 'Germany'
 
 --2
-SELECT name
-  FROM world
- WHERE population > 200000000
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
 --3
-SELECT name, gdp/population
-FROM world
-WHERE population > 200000000
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
