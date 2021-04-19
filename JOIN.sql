1eb1fe309bd3c60049655b0105ffca10630f376f
JimmyMcKegger <57632668+JimmyMcKegger@users.noreply.github.com>
2021-04-19 23:24:01 +0100

    update

--- JOIN.sql
+++ JOIN.sql
@@ -14,3 +14,41 @@ SELECT player, teamid, stadium, mdate
     JOIN goal 
     ON (game.id = goal.matchid)
   WHERE teamid='GER'
+
+--4
+SELECT game.team1, game.team2, goal.player
+  FROM game
+    JOIN goal 
+    ON (game.id = goal.matchid)
+  WHERE goal.player LIKE 'Mario%'
+
+--5
+SELECT g.player, g.teamid, e. coach, g.gtime
+  FROM goal g
+JOIN eteam e
+ON g.teamid = e.id
+ WHERE gtime<=10
+
+--6
+SELECT g.mdate, e.teamname
+FROM game g
+JOIN eteam e
+ON g.team1 = e.id
+WHERE e.coach = 'Fernando Santos'
+
+--7
+SELECT go.player
+FROM goal go
+JOIN game ga
+ON go.matchid = ga.id
+WHERE ga.stadium = 'National Stadium, Warsaw'
+
+--8
+SELECT DISTINCT o.player
+  FROM goal o 
+JOIN game a
+ON id = matchid
+WHERE (a.team1='GER' OR a.team2='GER')
+AND o.player IN (SELECT player from goal x WHERE teamid != 'GER')
+
+--9
